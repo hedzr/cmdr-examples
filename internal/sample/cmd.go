511e6b83b071c01ee0c386b96e72bbf033a62350
Hedzr Yeh <hedzrz@gmail.com>
2020-05-26 12:39:39 +0800

    added `sample`
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

---
+++ internal/sample/cmd.go
@@ -0,0 +1,28 @@
+// Copyright © 2020 Hedzr Yeh.
+
+package sample
+
+import "github.com/hedzr/cmdr"
+
+// WithSampleCmdrOption show the example howto modify cmdr daemon plugin `server` `start` sub-command at an appropriate time.
+func WithSampleCmdrOption() cmdr.ExecOption {
+	return func(w *cmdr.ExecWorker) {
+		// daemonImpl = daemonImplX
+
+		w.AddOnBeforeXrefBuilding(func(root *cmdr.RootCommand, args []string) {
+			// the following statements show you how to attach an option to a sub-command
+			var serverCmd, serverStartCmd *cmdr.Command
+			serverCmd = cmdr.FindSubCommandRecursive("server", nil)
+			if serverCmd != nil {
+				serverStartCmd = cmdr.FindSubCommand("start", serverCmd)
+			}
+			if serverStartCmd != nil {
+				cmdr.NewInt(5100).
+					Titles("vnc-server",  "vnc").
+					Description("start as a vnc server (just a demo)").
+					Placeholder("PORT").
+					AttachTo(cmdr.NewCmdFrom(serverStartCmd))
+			}
+		})
+	}
+}
