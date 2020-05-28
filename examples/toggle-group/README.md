a11e1f006183ba43ad0bda8e790d77bbb6919bec
Hedzr Yeh <hedzrz@gmail.com>
2020-05-29 01:23:38 +0800

    added examples: `toggle-group`
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

---
+++ examples/toggle-group/README.md
@@ -0,0 +1,10 @@
+# toggle-group
+
+
+```bash
+go run ./examples/toggle-group tg --help
+go run ./examples/toggle-group tg
+go run ./examples/toggle-group tg --gin
+go run ./examples/toggle-group tg --echo
+go run ./examples/toggle-group tg --iris
+```
---
+++ examples/toggle-group/main.go
@@ -0,0 +1,77 @@
+// Copyright © 2020 Hedzr Yeh.
+
+package main
+
+import (
+	"fmt"
+	"github.com/hedzr/cmdr"
+	cmdr_examples "github.com/hedzr/cmdr-examples"
+)
+
+func main() {
+	Entry()
+}
+
+func Entry() {
+	if err := cmdr.Exec(buildRootCmd()); err != nil {
+		fmt.Printf("error: %+v\n", err)
+	}
+}
+
+func buildRootCmd() (rootCmd *cmdr.RootCommand) {
+	root := cmdr.Root(appName, cmdr_examples.Version).
+		Copyright(copyright, "hedzr").
+		Description(desc, longDesc).
+		Examples(examples)
+	rootCmd = root.RootCommand()
+
+	cmdr.NewBool(false).Titles("ueh", "enable-ueh", "Enables the unhandled exception handler?")
+
+	tg(root)
+	panicTest(root)
+
+	return
+}
+
+func tg(root cmdr.OptCmd) {
+	// toggle-group
+
+	c := root.NewSubCommand("toggle-group", "tg").
+		Description("soundex test").
+		Group("Test").
+		TailPlaceholder("[text1, text2, ...]").
+		Action(func(cmd *cmdr.Command, args []string) (err error) {
+			selectedMuxType := cmdr.GetStringR("toggle-group.mux-type")
+			fmt.Printf("Flag 'echo' = %v\n", cmdr.GetBoolR("toggle-group.echo"))
+			fmt.Printf("Flag 'gin' = %v\n", cmdr.GetBoolR("toggle-group.gin"))
+			fmt.Printf("Flag 'gorilla' = %v\n", cmdr.GetBoolR("toggle-group.gorilla"))
+			fmt.Printf("Flag 'iris' = %v\n", cmdr.GetBoolR("toggle-group.iris"))
+			fmt.Printf("Flag 'std' = %v\n", cmdr.GetBoolR("toggle-group.std"))
+			fmt.Printf("Toggle Group 'mux-type' = %v\n", selectedMuxType)
+			return
+		})
+
+	cmdr.NewBool(false).Titles("echo", "echo").ToggleGroup("mux-type").Group("Mux").AttachTo(c)
+	cmdr.NewBool(false).Titles("gin", "gin").ToggleGroup("mux-type").Group("Mux").AttachTo(c)
+	cmdr.NewBool(false).Titles("gorilla", "gorilla").ToggleGroup("mux-type").Group("Mux").AttachTo(c)
+	cmdr.NewBool(true).Titles("iris", "iris").ToggleGroup("mux-type").Group("Mux").AttachTo(c)
+	cmdr.NewBool(false).Titles("std", "std").ToggleGroup("mux-type").Group("Mux").AttachTo(c)
+}
+
+const (
+	appName   = "toggle-group"
+	copyright = "toggle-group is an effective devops tool"
+	desc      = "toggle-group is an effective devops tool. It make an demo application for `cmdr`."
+	longDesc  = "toggle-group is an effective devops tool. It make an demo application for `cmdr`."
+	examples  = `
+$ {{.AppName}} gen shell [--bash|--zsh|--auto]
+  generate bash/shell completion scripts
+$ {{.AppName}} gen man
+  generate linux man page 1
+$ {{.AppName}} --help
+  show help screen.
+`
+	overview = ``
+
+	zero = 0
+)
