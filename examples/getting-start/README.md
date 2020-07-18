6ff42fd762cc021ba1d5aae11e634e689ece2197
Hedzr Yeh <hedzrz@gmail.com>
2020-07-18 12:19:10 +0800

    sample: getting start
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

---
+++ examples/getting-start/README.md
@@ -0,0 +1,6 @@
+# Getting Start
+
+
+```bash
+go run ./examples/getting-start soundex just soso
+```
---
+++ examples/getting-start/main.go
@@ -0,0 +1,55 @@
+// Copyright © 2020 Hedzr Yeh.
+
+package main
+
+import (
+	"fmt"
+	"github.com/hedzr/cmdr"
+	cmdrexamples "github.com/hedzr/cmdr-examples"
+	"log"
+)
+
+func main() {
+	if err := cmdr.Exec(buildRootCmd()); err != nil {
+		log.Printf("error: %+v\n", err)
+	}
+}
+
+func buildRootCmd() (rootCmd *cmdr.RootCommand) {
+	root := cmdr.
+		Root(appName, cmdrexamples.Version).
+		Copyright(copyright, "hedzr").
+		Description(desc, longDesc).
+		Examples(examples)
+	rootCmd = root.RootCommand()
+	soundex(root)
+	return
+}
+
+func soundex(root cmdr.OptCmd) {
+	root.NewSubCommand("soundex", "snd", "sndx", "sound").
+		Description("soundex test").
+		Group("Test").
+		TailPlaceholder("[text1, text2, ...]").
+		Action(func(cmd *cmdr.Command, args []string) (err error) {
+			for ix, s := range args {
+				fmt.Printf("%5d. %s => %s\n", ix, s, cmdr.Soundex(s))
+			}
+			return
+		})
+}
+
+const (
+	appName   = "getting-start"
+	copyright = "getting-start is an effective devops tool"
+	desc      = "getting-start is an effective devops tool. It make an demo application for `cmdr`."
+	longDesc  = "getting-start is an effective devops tool. It make an demo application for `cmdr`."
+	examples  = `
+$ {{.AppName}} gen shell [--bash|--zsh|--auto]
+  generate bash/shell completion scripts
+$ {{.AppName}} gen man
+  generate linux man page 1
+$ {{.AppName}} --help
+  show help screen.
+`
+)
